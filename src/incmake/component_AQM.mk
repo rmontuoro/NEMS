787d60ae5d2c37316b081ea92460a8d8a3546e36
Raffaele Montuoro <raffaele.montuoro@noaa.gov>
2020-07-17 19:10:18 +0000

    Add standard debug flag for AQM component.

--- src/incmake/component_AQM.mk
+++ src/incmake/component_AQM.mk
@@ -9,11 +9,9 @@ AQM_BINDIR?=$(ROOTDIR)/AQM_INSTALL
 # Make sure the expected directories exist and are non-empty:
 $(call require_dir,$(AQM_SRCDIR),AQM source directory)
 
-AQM_ALL_OPTS= \
-  COMP_SRCDIR="$(AQM_SRCDIR)" \
-  COMP_BINDIR="$(AQM_BINDIR)" \
-  FMS_BINDIR="$(FMS_BINDIR)" \
-  MACHINE_ID="$(MACHINE_ID)"
+ifneq (,$(findstring DEBUG=Y,$(AQM_MAKEOPT)))
+  override AQM_CONFOPT += --enable-debug
+endif
 
 ########################################################################
 
@@ -22,15 +20,14 @@ AQM_ALL_OPTS= \
 build_AQM: $(aqm_mk)
 
 $(aqm_mk): configure
-	$(MODULE_LOGIC) ; export $(AQM_ALL_OPTS)                  ; \
-	set -e                                                        ; \
-	cd $(AQM_SRCDIR)                                          ; \
-	./configure --prefix=$(AQM_BINDIR)                          \
-	  --datarootdir=$(AQM_BINDIR) --libdir=$(AQM_BINDIR)
-	+$(MODULE_LOGIC) ; cd $(AQM_SRCDIR) ; exec $(MAKE)             \
-	  $(AQM_ALL_OPTS)
-	+$(MODULE_LOGIC) ; cd $(AQM_SRCDIR) ; exec $(MAKE)             \
-	  $(AQM_ALL_OPTS) install
+	$(MODULE_LOGIC) ; export $(AQM_ALL_OPTS)              ; \
+	set -e                                                ; \
+	cd $(AQM_SRCDIR)                                      ; \
+	./configure --prefix=$(AQM_BINDIR)                      \
+	  --datarootdir=$(AQM_BINDIR) --libdir=$(AQM_BINDIR)    \
+	  $(AQM_CONFOPT)
+	+$(MODULE_LOGIC) ; cd $(AQM_SRCDIR) ; exec $(MAKE)
+	+$(MODULE_LOGIC) ; cd $(AQM_SRCDIR) ; exec $(MAKE) install
 	test -d "$(AQM_BINDIR)"
 
 ########################################################################
